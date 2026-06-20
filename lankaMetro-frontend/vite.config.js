1ebb23b93e8db1bad95ebea34b4535bb42627534
Manjeedan <shakthivelmanjeedan11@gmail.com>
2026-06-20 21:40:14 +0530

    fix : add react plugin on viteJs

--- lankaMetro-frontend/vite.config.js
+++ lankaMetro-frontend/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    cssMinify: "esbuild", // 👈 Use esbuild instead of Lightning CSS
+    cssMinify: "esbuild", // Fix the Lightning CSS issue
   },
 });
