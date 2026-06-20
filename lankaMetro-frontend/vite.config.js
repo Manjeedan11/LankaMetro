049ec7f175f528d3ae9ad48a7849042d1e2324d3
Manjeedan <shakthivelmanjeedan11@gmail.com>
2026-06-20 21:38:07 +0530

    fix : vercel json's path and vite's build

--- lankaMetro-frontend/vite.config.js
+++ lankaMetro-frontend/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import path from "path";
 
@@ -10,5 +10,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    cssMinify: "esbuild", // 👈 Use esbuild instead of Lightning CSS
   },
 });
